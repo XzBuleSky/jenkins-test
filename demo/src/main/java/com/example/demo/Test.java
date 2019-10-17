11068a4c1f1b6491a5560a6de872689f05090581
xzzzj <694188152@qq.com>
2019-10-17 14:11:41 +0800

    test
    test 20191017 1411

---
+++ demo/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 写点注释吧~
+ *
+ * @author liubaofeng
+ * @date 2019/10/17
+ */
+@RestController
+public class Test {
+
+
+    @GetMapping("/demo")
+    public String test() {
+        return "Hello World";
+    }
+
+}
